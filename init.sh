347d5e2ac97bbefb47b0eddfb4d4790a2f4a3e6a
Eric Lagos <lakes1126@gmail.com>
2024-01-18 16:36:12 +0100

    Add tmux and tmux-sessionizer

--- init.sh
+++ init.sh
@@ -19,10 +19,11 @@ link $HOME/.dotfiles/.skhdrc $HOME/.skhdrc
 # .config
 link $HOME/.dotfiles/nvim $HOME/.config/nvim
 link $HOME/.dotfiles/kitty $HOME/.config/kitty
+link $HOME/.dotfiles/tmux/ $HOME/.config/tmux
 
 # directories
 link $HOME/.dotfiles/zshrc_private $HOME/.zshrc_private
-
+link $HOME/.dotfiles/bin $HOME/bin
 
 # link $HOME/.dotfiles to PWD
 link $(pwd) $HOME/.dotfiles
