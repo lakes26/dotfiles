9201d578e69693033ba24d6531248827cdc5de7f
Eric Lagos <lakes1126@gmail.com>
2024-02-24 18:58:54 -0500

    changed harpoon mappings

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -6,12 +6,12 @@ harpoon:setup()
 -- REQUIRED
 
 vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
-vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+vim.keymap.set("n", "<C-s>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<leader>nn", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<leader>ee", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<leader>ii", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<leader>oo", function() harpoon:list():select(4) end)
 
 -- Toggle previous & next buffers stored within Harpoon list
 vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
@@ -21,10 +21,10 @@ wk.register({
     ["<leader>a"] = 'Append to harpoon list',
     ["<C-E>"] = 'Show harpoon list',
 
-    ["<C-h>"] = 'Goto 1st file in harpoon list',
-    ["<C-j>"] = 'Goto 2nd file in harpoon list',
-    ["<C-k>"] = 'Goto 3rd file in harpoon list',
-    ["<C-l>"] = 'Goto 4th file in harpoon list',
+    ["<leader>nn"] = 'Goto 1st file in harpoon list',
+    ["<leader>ee"] = 'Goto 2nd file in harpoon list',
+    ["<leader>ii"] = 'Goto 3rd file in harpoon list',
+    ["<leader>oo"] = 'Goto 4th file in harpoon list',
 
     ["<C-S-P>"] = 'Goto previous file in harpoon list',
     ["<C-S-N>"] = 'Goto next file in harpoon list',
