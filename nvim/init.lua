7dab49799ac99407ff187bdbb5057862a0c54ddb
Eric Lagos <lakes1126@gmail.com>
2024-03-04 15:23:57 -0500

    removed sessionize vim shortcut

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,7 +9,6 @@ wk.register({
     ["<leader>y"] = "Copy to system clipboard",
     ["<leader>Y"] = "Copy line to system clipboard",
     ["<leader>d"] = "Delete to void register",
-    ["<C-f>"] = "Tmux sessionizer",
     ["<leader>fo"] = "Format the current buffer using LSP",
     ["<leader>s"] = "Subsitute the current word in buffer",
     ["<leader>mx"] = "Make current file executable",
